6000241c80bddcb06f95280345822c3a0aab9a1a
muxammadbek <muxammadbekbotirbek@gmail.com>
2024-12-14 01:09:09 -0800

    Konfliktlar hal qilindi

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AppRoutes from "./routes";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
+import Dashboard2 from "./Client/dashboard2.tsx";
 
 const App: React.FC = () => {
   return (
     <div className="bg-gray-100 min-h-screen">
       <BrowserRouter>
         <AppRoutes />
+          <Route path="/dashboard2" element={<Dashboard2 />} />
       </BrowserRouter>
 
     </div>
