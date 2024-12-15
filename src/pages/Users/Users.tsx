974f70f03c046c65d83889f4e6f5cd1e7a03e8ff
@MingboyevAlisher2009 <mingboyevalisher171@gmail.com>
2024-12-15 12:31:43 +0500

    client

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const Users: React.FC = () => {
-  return (
-    <div className="p-4 bg-white shadow rounded">
-      <h2 className="text-lg font-bold mb-4">Foydalanuvchilar</h2>
-      <p>Bu bo'limda foydalanuvchilarni boshqarish mumkin.</p>
-    </div>
-  );
+	return (
+		<div className='p-4 bg-white shadow rounded'>
+			<h2 className='text-lg font-bold mb-4'>Foydalanuvchilar</h2>
+			<p>Bu bo'limda foydalanuvchilarni boshqarish mumkin.</p>
+		</div>
+	)
 }
+export default Users
