cb56d1ea091e3437e0961608d9ca547899bd61f1
amirbekk7 <normurodovamirbek3@gmail.com>
2024-12-21 19:54:02 +0500

    login xato tugirlandi

--- src/auth/LoginForm/login.tsx
+++ src/auth/LoginForm/login.tsx
@@ -18,7 +18,7 @@ function Login() {
 
   const handleSubmit = async () => {
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    
+
     if (!emailRegex.test(email)) {
       setEmailError("Iltimos, to'g'ri email manzilini kiriting.");
       return;
@@ -50,12 +50,14 @@ function Login() {
           position: "top-center",
           autoClose: 2000,
         });
-        setResData(response.data); // API javobini saqlash
+        setResData(response.data);
+        console.log(resData);
+
 
       } else {
         setError("Email yoki parol noto'g'ri.");
       }
-      
+
     } catch (error) {
       console.error(error);
       setError("Tizimda xatolik yuz berdi, iltimos qayta urinib ko'ring.");
@@ -64,21 +66,25 @@ function Login() {
     }
   };
 
-  // useEffect faqat resData o'zgarganda ishga tushadi
-  useEffect(() => {
+
+  const CheckRole = () => {
     if (resData) {
-      setResData(null); // resData ni qayta null qilish
+      setResData(null);
       setEmail('');
       setPassword('');
       const role = localStorage.getItem('role');
-      
+
       if (role === 'ROLE_SUPER_ADMIN') navigate('/result');
       if (role === 'ROLE_TESTER') navigate('/tester-dashboard');
       if (role === 'ROLE_ADMIN') navigate('/admin-dashboard');
       if (role === 'ROLE_CLIENT') navigate('/result');
-      else navigate('/register');
+      else navigate('/login');
     }
-  }, [resData]); // resData o'zgarganda faqat ishlaydi
+  }
+
+  useEffect(()=>{
+    CheckRole()
+  },[CheckRole()])
 
   const isLoginButtonDisabled = !(email && password) || !!error || !!emailError || isLoading;
 
